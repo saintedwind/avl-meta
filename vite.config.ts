d2e0577d1ae219a4292cc7cc0af237c8ef177067
saintedwind <peymangholamhosseini@gmail.com>
2025-10-20 11:20:50 +0330

    fix: valid package.json and deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc"; // <-- use -swc here
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
 });
